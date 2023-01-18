5828e1bab9abd1b074b7d3858ab0088985d9157d
kunal <kunal@kunals-MacBook-Pro.local>
2023-01-18 11:37:57 +0530

    initial commit for the portfolio website

--- constants/data.js
+++ constants/data.js
@@ -3,7 +3,7 @@ const userData = {
   name: "Manu Arora",
   designation: "Full-Stack Developer",
   avatarUrl: "/avatar.png",
-  email: "manuarorawork@gmail.com",
+  email: "manuarorawork@gmail.com", 
   phone: "+91 9587738861",
   address: "Jaipur, Rajasthan, India.",
   projects: [
